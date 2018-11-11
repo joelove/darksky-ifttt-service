ce45bb12e4b4477a56d8813da7c98c709c1af635
Joe Love <git@joelove.uk>
2018-11-11 19:03:37 +0000

    update migrations to use arrow functions

--- migrations/20160319145300_addSamplesTable.js
+++ migrations/20160319145300_addSamplesTable.js
@@ -1,13 +1,13 @@
-exports.up = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.createTable('samples', function(table) {
+exports.up = (knex, Promise) => (
+  Promise.all([
+    knex.schema.createTable('samples', (table) => {
       table.uuid('id').notNullable().primary();
       table.string('page').notNullable();
       table.integer('count').defaultTo(0);
     }),
-  ]);
-};
+  ])
+);
 
-exports.down = function(knex, Promise) {
+exports.down = (knex) => {
   knex.schema.dropTable('samples');
 };
